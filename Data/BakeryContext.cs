197f988898a500154cb2eba0c25020cd9eebbd7d
Clint Kuhl <kuhl.clint@gmail.com>
2021-01-14 22:50:19 -0600

    Add project files.

---
+++ Data/BakeryContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Bakery.Models;
+using Bakery.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bakery.Data
+{
+    public class BakeryContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data source=Bakery.db");
+        }
+    }
+}
